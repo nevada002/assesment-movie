d2c43eafacabd42b65a570396b4baf0d948d0bc6
nevada002 <daniel.latuary@gmail.com>
2024-06-29 17:31:12 +0700

    "Updated title in index.html, removed App.jsx, and modified Header and Watermark components"

--- src/components/Layouts/Watermark.jsx
+++ src/components/Layouts/Watermark.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import styles from './Layouts.module.css'
 
 export default function Watermark() {
     return (
         <div>
-
+            <span className={styles.Watermark}>Created by Juliant Daniel</span>
         </div>
     )
 }
